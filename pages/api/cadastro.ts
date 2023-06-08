7fcc2f0b770a4f49b7237ff83dfcc72f949d15ed
caca endlich <camilaendlichpimenta.trab@gmail.com>
2023-06-08 19:02:55 -0300

    Implementar endpoint de login com conexão ao MongoDB e autenticação

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -1,12 +1,44 @@
-import type {NextApiRequest, NextApiResponse} from "next";
-import { respostaPadraoMsg } from "../../types/respostaPadraoMsg";
-import { cadastroRequisicao } from "../../types/cadastroRequisicao";
-import { METHODS } from "http";
-import { error } from "console";
+import type {NextApiRequest, NextApiResponse} from 'next';
+import { respostaPadraoMsg } from '../../types/respostaPadraoMsg';
+import { cadastroRequisicao } from '../../types/cadastroRequisicao';
+import {UsuarioModel} from '../../models/usuarioModel';
+import {conectarMongpDB} from '../../middlewares/conectarMongoDB';
+import md5 from 'md5';
 
-const endpointCadastro = (req: NextApiRequest, res: NextApiResponse) => {
+const endpointCadastro = async (req: NextApiRequest, res: NextApiResponse<respostaPadraoMsg>) => {
+    //validaçãoas
     if (req.method === 'POST') {
+        const usuario = req.body as (cadastroRequisicao);
 
+        if (!usuario.nome || usuario.nome.length < 2){
+            return res.status(400).json({error: 'Nome inválido.'});
+        }
+
+        if (!usuario.email || usuario.email.length < 5 || !usuario.email.includes('@') || !usuario.email.includes('.')){
+            return res.status(400).json({error: 'Email inválido.'});
+        }
+
+        if (!usuario.senha || usuario.senha.length < 4 ){
+            return res.status(400).json({error: 'Senha inválido.'});
+        }
+
+        // validação se já existe usuário com o mesmo e-mail
+        const usuariosComOMesmoEmail = await UsuarioModel.find({email: usuario.email});
+        if (usuariosComOMesmoEmail && usuariosComOMesmoEmail.length > 0){
+            return res.status(400).json({error: 'Já existe uma conta com o email informado.'});
+
+        }
+
+        // salvar no banco de dados
+        const UsuarioASerSalvo = {
+            nome : usuario.nome,
+            email : usuario.email,
+            senha : md5(usuario.senha)
+        }
+        await UsuarioModel.create(UsuarioASerSalvo);
+        return res.status(200).json({msg: 'Usuário cadastrado com sucesso.'});
     }
     return res.status(405).json({error: 'Método informado não é válido.'});
 }
+
+export default conectarMongpDB(endpointCadastro);
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,16 +1,21 @@
 import type{NextApiRequest, NextApiResponse} from 'next';
 import { conectarMongpDB } from '../../middlewares/conectarMongoDB';
 import type {respostaPadraoMsg} from '../../types/respostaPadraoMsg';
+import { UsuarioModel } from '../../models/usuarioModel';
+import md5 from 'md5';
 
-    const endpointLogin = (req: NextApiRequest, res: NextApiResponse<respostaPadraoMsg>) => {
+    const endpointLogin = async (req: NextApiRequest, res: NextApiResponse<respostaPadraoMsg>) => {
         if (req.method === 'POST') {
-            const {login, senha} = req.body;
+            const {login, senha} = req.body; // quando o usuário interage com o sistema e fornece seu email e senha, esses valores são capturados no código através dessa linha.
+            const usuariosEncontrados = await UsuarioModel.find({email: login, senha: md5(senha)});
 
-            if (login === 'admin@admin.com' && senha === 'admin123'){
-                return res.status(200).json({msg: 'Usuário autenticado com sucesso.'});
+            if (usuariosEncontrados && usuariosEncontrados.length > 0){
+                //Se um usuário for encontrado, obtém o primeiro usuário (usuariosEncontrados[0])
+                const usuarioEncontrado = usuariosEncontrados[0];
+                return res.status(200).json({msg: `Usuário ${usuarioEncontrado.nome} autenticado com sucesso.`});
             } return res.status(400).json({error: 'Usuário ou Senha não encontrado.'});
 
         } return res.status(405).json({error: 'Método informado não é válido.'});
     }
 
-export default conectarMongpDB (endpointLogin);
+export default conectarMongpDB (endpointLogin); //Exporta a função endpointLogin envolta na função conectarMongpDB, que é usada para conectar ao MongoDB antes de executar a função do endpoint.
