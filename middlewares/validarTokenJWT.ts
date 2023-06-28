d4aa394f236361777543e3a19f1b5ab79575475d
caca endlich <camilaendlichpimenta.trab@gmail.com>
2023-06-27 21:13:19 -0300

    Edit msgm de erro

--- middlewares/validarTokenJWT.ts
+++ middlewares/validarTokenJWT.ts
@@ -1,4 +1,4 @@
-import type {NextApiRequest, NextApiResponse, NextApiHandler } from "next";
+import type {NextApiRequest, NextApiResponse, NextApiHandler } from "next"; // NextApiHandler é manipulador de API Next.js
 import type {respostaPadraoMsg} from '../types/respostaPadraoMsg';
 import jwt, {JwtPayload} from "jsonwebtoken";
 
@@ -14,24 +14,24 @@ export const validarTokenJWT = (handler: NextApiHandler) => async (req: NextApiR
 
         if (!req || !req.headers){
             // req.headers (que contém os cabeçalhos da requisição), nesse caso validando se são nulos.
-            return res.status(401).json({error: 'Não foi possível validar o otken de acesso.'});
+            return res.status(401).json({error: 'Não foi possível validar o token de acesso.'});
         }
 
         if (req.method !== 'OPTIONS'){
 
             const authorization = req.headers['authorization'];
             if (!authorization){
-            return res.status(401).json({error: 'Não foi possível validar o otken de acesso.'});
+                return res.status(401).json({error: 'Não foi possível validar o token de acesso.'});
             }
 
             const token = authorization.substring(7);
             if (!token){
-            return res.status(401).json({error: 'Não foi possível validar o otken de acesso.'})
+                return res.status(401).json({error: 'Não foi possível validar o token de acesso.'});
             }
 
             const decoded = await jwt.verify(token, MINHA_CHAVE_JWT) as JwtPayload;
             if (!decoded){
-            return res.status(401).json({error: 'Não foi possível validar o otken de acesso.'})
+                return res.status(401).json({error: 'Não foi possível validar o token de acesso.'});
             }
 
             if (!req.query){
@@ -43,7 +43,7 @@ export const validarTokenJWT = (handler: NextApiHandler) => async (req: NextApiR
 
     } catch(e) {
         console.log(e);
-        return res.status(401).json({error: 'Não foi possível validar o otken de acesso.'})
+        return res.status(401).json({error: 'Não foi possível validar o token de acesso.'});
 
     }
 
