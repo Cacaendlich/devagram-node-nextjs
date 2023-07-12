109113baa8e5656d38a454cc1964b59e574e5982
caca endlich <camilaendlichpimenta.trab@gmail.com>
2023-07-12 17:42:34 -0300

    comentário

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,4 +1,4 @@
-//Rota de teste
+//Perfil
 //quando essa rota é acessada, o middleware validarTokenJWT verifica o token JWT e, se for válido, a função usuarioEndpoint é executada e retorna a mensagem de sucesso.
 import { NextApiResponse, NextApiRequest } from "next";
 import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
